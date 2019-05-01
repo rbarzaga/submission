e8f1b0917ad3c559df3fd3a77fae73ca96b66cd0
rbarzaga <rbarzaga@yahoo.com>
2019-05-01 12:30:28 -0700

    happy path espresso test

---
+++ TopAlbums/app/src/androidTest/java/com/example/topalbums/MainActivityTest.java
@@ -0,0 +1,81 @@
+package com.example.topalbums;
+
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction constraintLayout = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.albums_recycler_view),
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0)),
+                        0),
+                        isDisplayed()));
+        constraintLayout.perform(click());
+
+        pressBack();
+
+        ViewInteraction constraintLayout2 = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.albums_recycler_view),
+                                childAtPosition(
+                                        withClassName(is("android.widget.LinearLayout")),
+                                        0)),
+                        1),
+                        isDisplayed()));
+        constraintLayout2.perform(click());
+
+        pressBack();
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
